c5b6474208f6537e099bf7d73fd7c75ec5e15778
root <root@Baffoe6.>
2025-07-10 12:21:12 +0200

    Initial calculator interface with stubs

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# calculator.py
+
+def add(a, b):
+    pass  # To be implemented by Developer 1
+
+def subtract(a, b):
+    pass  # To be implemented by Developer 2
+
+def multiply(a, b):
+    pass  # To be implemented by Developer 3
+
+def divide(a, b):
+    pass  # To be implemented by Developer 4
+
+if __name__ == "__main__":
+    print("Calculator Interface Ready")
